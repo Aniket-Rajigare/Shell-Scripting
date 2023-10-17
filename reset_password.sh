2555ab60eb8ac6f790576cd62f6818b8d5542611
Aniket-Rajigare <aniket.rajigare@gmail.com>
2023-10-17 12:12:54 +0530

    my first shell program

---
+++ reset_password.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -sp "current password : " cp
+echo
+read -sp "Enter new password : " np
+echo
+read -sp "Re-enter your password : " rp
+echo
+echo "your new password is set to $np"
+
